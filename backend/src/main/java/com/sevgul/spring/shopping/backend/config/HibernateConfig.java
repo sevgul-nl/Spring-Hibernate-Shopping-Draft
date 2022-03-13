f1f30b55a4e4ce021c4aa03e41f7deae1891c288
bakis-nl <baki.sevgul@gmail.com>
2022-03-13 05:08:37 +0300

    dockerizing

--- backend/src/main/java/com/sevgul/spring/shopping/backend/config/HibernateConfig.java
+++ backend/src/main/java/com/sevgul/spring/shopping/backend/config/HibernateConfig.java
@@ -22,8 +22,8 @@ public class HibernateConfig {
 	//private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/boncuk"; jdbc:h2:[file:][<path>]
 	//jdbc:h2:tcp://<server>[:<port>]/[<path(/Users/bakimac/)>]<databaseName>
 	//private final static String DATABASE_URL = "jdbc:h2:file:/Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/h2-data/myh2";
-	//private final static String DATABASE_URL = "jdbc:h2:tcp://localhost:9092/~/h2-data/myh2";
-	private final static String DATABASE_URL = "jdbc:h2:/~/h2-data/myh2";
+	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost:9092/~/h2-data/myh2";
+	//private final static String DATABASE_URL = "jdbc:h2:/~/h2-data/myh2";
 	private final static String DATABASE_DRIVER = "org.h2.Driver";
 	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
 	private final static String DATABASE_USER = "sa";
