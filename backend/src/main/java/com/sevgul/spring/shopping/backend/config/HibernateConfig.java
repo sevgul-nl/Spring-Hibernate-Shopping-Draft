2ed2a1fd109d50a607cf77314ffc334dbce52116
Baki Sevgul <bakimac@Bakis-MBP.home>
2023-02-19 17:03:19 +0100

    h2

--- backend/src/main/java/com/sevgul/spring/shopping/backend/config/HibernateConfig.java
+++ backend/src/main/java/com/sevgul/spring/shopping/backend/config/HibernateConfig.java
@@ -24,7 +24,8 @@ public class HibernateConfig {
 	// doker jdbc:h2:tcp://my-h2/my-db-name
 	//private final static String DATABASE_URL = "jdbc:h2:file:/Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/h2-data/myh2";
 	//private final static String DATABASE_URL = "jdbc:h2:file:/Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/backend/src/main/resources/db/shopping";
-	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost//Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/backend/src/main/resources/db/shopping";
+	//private final static String DATABASE_URL = "jdbc:h2:tcp://localhost//Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/backend/src/main/resources/db/shopping";
+	private final static String DATABASE_URL = "jdbc:h2:tcp://backend/src/main/resources/db/shopping";
 	//private final static String DATABASE_URL = "jdbc:h2:tcp://h2-data/myh2";
     //private final static String DATABASE_URL = "jdbc:h2:/h2-data/myh2";
 	private final static String DATABASE_DRIVER = "org.h2.Driver";
