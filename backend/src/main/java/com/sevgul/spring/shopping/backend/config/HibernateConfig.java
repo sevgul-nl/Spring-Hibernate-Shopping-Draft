6380dc99c6c7a4c6c7b2328bf64e955cf91d2533
Baki Sevgul <bakimac@Bakis-MBP.home>
2023-02-22 17:50:59 +0100

    h2 db

--- backend/src/main/java/com/sevgul/spring/shopping/backend/config/HibernateConfig.java
+++ backend/src/main/java/com/sevgul/spring/shopping/backend/config/HibernateConfig.java
@@ -25,8 +25,8 @@ public class HibernateConfig {
 	// doker jdbc:h2:tcp://my-h2/my-db-name
 	//private final static String DATABASE_URL ="jdbc:h2:/Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/h2-data/myh2;MODE=LEGACY";
 	//private final static String DATABASE_URL ="jdbc:h2:tcp://localhost//Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/h2-data/myh2;MODE=LEGACY";
-	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost//home/pi/h2-data/myh2;MODE=LEGACY";
-	//private final static String DATABASE_URL = "jdbc:h2:/home/pi/h2-data/myh2;MODE=LEGACY";
+	private final static String DATABASE_URL = "jdbc:h2:tcp://127.0.1.1//home/pi/h2-data/myh2;MODE=LEGACY";
+	//private final static String DATABASE_URL = "jdbc:h2:/home/pi/h2-data/myh2";
 
 	// private final static String DATABASE_URL =
 	// "jdbc:h2:file:/Users/bakimac/Documents/deve-works/wsSAShopping/SAShopping/backend/src/main/resources/db/shopping";
